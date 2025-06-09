b76aa2f70e8467c0bbd6d2e5f14241a405d19c70
DarryDerr <darryariel87@gmail.com>
2025-06-09 14:04:46 +0700

    Add files via upload

--- server/versioncheck.lua
+++ server/versioncheck.lua
@@ -1,15 +1,14 @@
-local currentVersion = "1.5"  -- Versi lokal yang sekarang
+local currentVersion = "1.5"
 
 local function checkVersion()
     PerformHttpRequest("https://raw.githubusercontent.com/DarryDerr/qb-slaughtererjob/main/version.txt", function(errorCode, result, headers)
         if errorCode == 200 then
-            local latestVersion = result:gsub("%s+", "") -- hapus whitespace
+            local latestVersion = result:gsub("%s+", "")
             if latestVersion ~= currentVersion then
                 print("^1[Version Check]^7 Update available! Current Version: " .. currentVersion .. " | Latest: " .. latestVersion)
                 TriggerEvent('chat:addMessage', {
                     args = {"^1[Update]^7 New version available: " .. latestVersion .. ". Please update your resource."}
                 })
-                -- Bisa juga trigger notif / popup dll di client
             else
                 print("^2[Version Check]^7 You have the latest version: " .. currentVersion)
             end
@@ -19,7 +18,6 @@ local function checkVersion()
     end, "GET")
 end
 
--- Panggil cek versi saat resource start
 AddEventHandler('onResourceStart', function(resourceName)
     if GetCurrentResourceName() == resourceName then
         checkVersion()
